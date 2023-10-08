ef865fc1690d9576043bdcd33b1c7c668495ade8
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-10-08 09:30:01 +0300

    fonts and color palette added.

---
+++ lib/view/on_boarding/startup_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StartupView extends StatefulWidget {
+  const StartupView({super.key});
+
+  @override
+  State<StartupView> createState() => _StartupViewState();
+}
+
+class _StartupViewState extends State<StartupView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(),
+    );
+  }
+}
